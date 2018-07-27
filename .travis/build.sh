87af07f67a0cd283963472faab48523377e94c50
Ilja Bobkevic <ilja@spotify.com>
2018-07-27 16:02:41 +0200

    Fix entrypoint

--- .travis/build.sh
+++ .travis/build.sh
@@ -7,7 +7,7 @@ for c in $(< commands.txt); do
   arr_c=(${c//:/ })
   arr_v=(${arr_c//=/ })
   arr_p=(${arr_c[1]//;/ })
-  echo -e "#!/usr/bin/env bash\nset -Eeuo pipefail\nexec ${arr_v} \"$@\"" > entrypoint.sh
+  echo -e "#!/usr/bin/env bash\nset -Eeuo pipefail\nexec ${arr_v} \"\$@\"" > entrypoint.sh
   if [ "x${tag}" = "xlatest" ]; then
     docker build --build-arg REQUIRED_PACKAGES="$(echo ${arr_p[@]})" -t ${docker_org}/${arr_v}:${tag} .
   else
