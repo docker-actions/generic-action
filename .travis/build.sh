bc94d2d25cec032c1ed34462dc0cde9cb3529702
Ilja Bobkevic <ilja@spotify.com>
2018-09-05 01:18:22 +0200

    Try forked builds

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,16 +1,47 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 docker_org="${1}"
 tag="${2}"
 
+declare -A build_pids
+
+echo "Worker: ${WORKER}. Max workers: ${MAX_WORKERS}"
+
+from_i=$(($(($WORKER-1))*$MAX_WORKERS))
+to_i=$(($WORKER*$MAX_WORKERS))
+i=0
+echo "From: ${from_i}; to: ${to_i}"
 for c in $(< commands.txt); do
-  arr_c=(${c//:/ })
-  arr_v=(${arr_c//=/ })
-  arr_p=(${arr_c[1]//;/ })
-  echo -e "#!/usr/bin/env bash\nset -Eeuo pipefail\nexec ${arr_v} \"\$@\"" > entrypoint.sh
-  if [ "x${tag}" = "xlatest" ]; then
-    docker build --build-arg REQUIRED_PACKAGES="$(echo ${arr_p[@]})" -t ${docker_org}/${arr_v}:${tag} .
+  i=$(($i+1))
+  if [ $i -gt $from_i -a $i -le $to_i ]; then
+    arr_c=(${c//:/ })
+    arr_v=(${arr_c//=/ })
+    arr_p=(${arr_c[1]//;/ })
+    echo -e "#!/usr/bin/env bash\nset -Eeuo pipefail\nexec ${arr_v} \"\$@\"" > entrypoint.sh
+    if [ "x${tag}" = "xlatest" ]; then
+      image_tag=${tag}
+    else
+      image_tag=${arr_v[1]}-${tag}
+    fi
+    echo Building ${arr_v}:${image_tag}
+    docker build --build-arg REQUIRED_PACKAGES="$(echo ${arr_p[@]})" -t ${docker_org}/${arr_v}:${image_tag} . >${arr_v}.log 2>&1 &
+    build_pids[${arr_v}]=$!
   else
-    docker build --build-arg REQUIRED_PACKAGES="$(echo ${arr_p[@]})" -t ${docker_org}/${arr_v}:${arr_v[1]}-${tag} .
+    echo "Index: ${i} does not belong to worker ${WORKER}"
   fi
+
+  if [ $i -eq $to_i ]; then
+    from_i=$(($from_i+$(($MAX_WORKERS*$MAX_WORKERS))))
+    to_i=$(($from_i+$MAX_WORKERS))
+    echo "New from: ${from_i}; to: ${to_i}"
+  fi
+done
+
+for n in "${!build_pids[@]}"; do
+  echo Waiting for $n to build
+  until tail --pid=${build_pids[$n]} -f ${n}.log; do
+    echo "Failed tailing ${n}.log. Retrying in 1 second..."
+    sleep 1
+  done
+  wait ${build_pids[$n]} && echo "Build complete for ${n}" || (echo "Failed building ${n}. Log output:" && cat ${n}.log && false)
 done
